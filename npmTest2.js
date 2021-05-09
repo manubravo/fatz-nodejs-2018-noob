5c6ec40a7c525229b16101368310eb097fe3d0f2
Manu Bravo <manubravostudying@gmail.com>
2021-05-09 22:48:54 +0200

    Testeando la excepción de NODE_MODULES.
    Este commit no debería subir el node_modules.
    Recuerda que para arrancar siempre un proyecto nuevo hay que darle:
    npm i (2 veces)
    npm start ó bien npm run start2.
    Todavía soy un novato de mierda con el Git, pero bueno poco a poco.

--- npmTest2.js
+++ npmTest2.js
@@ -4,11 +4,11 @@ const servidorExpress = express();
 
 // EXPRESS POR DEFECTO NOS OBLIGA A DEVOLVER ALGO O SINO NOS SALDRÁ ERROR--> CANNOT GET /
 servidorExpress.get('/', function (peticionRequest, respuestaResponse){
-    respuestaResponse.send('<h1>Hola Mundo de Javascript, ahora estamos usando Express.</h1>');
+    respuestaResponse.send('<h1>Hola Mundo de Javascript, ahora estamos usando Express v2.</h1>');
     respuestaResponse.end();
 })
 
 // el levantamiento del servidor siempre con ROLLBACK
 servidorExpress.listen(3000, function(){
-    console.log('Servicios levantados por el puerto 3000 usando EXPRESSJS'.green);
+    console.log('Servicios levantados por el puerto 3000 usando EXPRESSJS v2'.green);
 });
